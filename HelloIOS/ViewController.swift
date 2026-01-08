8d10e5cf126cf1f24a95cfefb40e23faaa4c1efb
Katharina Cheng <qiaoyc2@uw.edu>
2026-01-08 11:07:23 -0800

    Initial Commit

---
+++ HelloIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Katharina Cheng on 1/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloIOSTests/HelloIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSTests.swift
+//  HelloIOSTests
+//
+//  Created by Katharina Cheng on 1/8/26.
+//
+
+import Testing
+@testable import HelloIOS
+
+struct HelloIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
